6d325c2555cca462a0b9c0d38994e794d0e4221b
Achil <achil.isnard-ext@alstomgroup.com>
2025-05-15 16:57:55 +0200

    update

---
+++ scripts.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('.card').forEach(card => {
+    let isMouseDown = false, startX, startY;
+  
+    card.addEventListener('mousedown', (e) => {
+      isMouseDown = true;
+      startX = e.pageX;
+      startY = e.pageY;
+    });
+  
+    card.addEventListener('mouseup', () => {
+      isMouseDown = false;
+    });
+  
+    card.addEventListener('mousemove', (e) => {
+      if (!isMouseDown) return;
+  
+      const deltaX = e.pageX - startX;
+      const cardInner = card.querySelector('.inner-card');
+      cardInner.style.transform = `rotateY(${deltaX}deg)`;
+    });
+  
+    card.addEventListener('mouseleave', () => {
+      isMouseDown = false;
+    });
+  });
